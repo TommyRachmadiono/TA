0506b6f6d3cd0b05db00860f202ee1302721b494
pratamaaudi <pratamaaudi@gmail.com>
2018-03-17 00:00:59 +0700

    fix load setelah konten habis
    - cek lastID, kalau 0 berarti konten sudah habis

--- js/scroll_feeds.js
+++ js/scroll_feeds.js
@@ -4,10 +4,12 @@ $(document).ready(function(){
         var lastID = $('#postterakhir').attr('lastID');
         var groupID = $('#postterakhir').attr('groupID');
         var act = $('#postterakhir').attr('act');
-        // alert(lastID);
-        // alert(groupID);
-        // alert(act);
-        $.ajax({
+        // alert("lastid = "+lastID);
+        // alert("groupid = "+groupID);
+        // alert("act = "+act);
+
+        if(lastID!=0){
+            $.ajax({
                 type:'POST',
                 url:'getData.php',
                 data:{id:+lastID,
@@ -21,6 +23,7 @@ $(document).ready(function(){
                     $('#postlist').append(html);
                 }
             });
+        }
     });
     // $(window).scroll(function(){
     //     // alert($(window).scrollTop()+" == "  +($(document).height()-$(window).height()));
